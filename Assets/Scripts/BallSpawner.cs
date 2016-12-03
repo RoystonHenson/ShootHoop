d77ff91e01f8c4574df4606143085777adaeebf0
Royston Henson <rlqhenson@hotmail.com>
2016-12-03 16:06:55 +0000

    added direction and velocity to basketballs

--- Assets/Scripts/BallSpawner.cs
+++ Assets/Scripts/BallSpawner.cs
@@ -11,9 +11,25 @@ public class BallSpawner : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		KeyCode mousePrimary = KeyCode.Mouse0;
-		if(Input.GetKeyDown(mousePrimary)) {
-			Instantiate(ballPrefab);
+		if(Input.GetKeyDown(KeyCode.UpArrow)) {
+			GameObject instance = Instantiate(ballPrefab);
+			Rigidbody rb = instance.GetComponent<Rigidbody>();
+			rb.velocity = Vector3.forward;
+		}
+		if(Input.GetKeyDown(KeyCode.DownArrow)) {
+			GameObject instance = Instantiate(ballPrefab);
+			Rigidbody rb = instance.GetComponent<Rigidbody>();
+			rb.velocity = Vector3.back;
+		}
+		if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+			GameObject instance = Instantiate(ballPrefab);
+			Rigidbody rb = instance.GetComponent<Rigidbody>();
+			rb.velocity = Vector3.left * 20.0f;
+		}
+		if(Input.GetKeyDown(KeyCode.RightArrow)) {
+			GameObject instance = Instantiate(ballPrefab);
+			Rigidbody rb = instance.GetComponent<Rigidbody>();
+			rb.velocity = Vector3.right * 5.0f;
 		}
 	}
 }
