2ed2acb74137ec0028735b14d7568030a5baf4cf
Royston Henson <rlqhenson@hotmail.com>
2016-12-03 15:45:34 +0000

    able to spawn basketballs with primary mouse

---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallSpawner : MonoBehaviour {
+
+	public GameObject ballPrefab;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		KeyCode mousePrimary = KeyCode.Mouse0;
+		if(Input.GetKeyDown(mousePrimary)) {
+			Instantiate(ballPrefab);
+		}
+	}
+}
