58c2f19085c4271b94ad8477201db97367f206e6
Royston Henson <rlqhenson@hotmail.com>
2016-12-03 16:12:42 +0000

    added correct code for velocity and direction

--- Assets/Scripts/BallSpawner.cs
+++ Assets/Scripts/BallSpawner.cs
@@ -11,25 +11,26 @@ public class BallSpawner : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		float velocity = 20f;
 		if(Input.GetKeyDown(KeyCode.UpArrow)) {
 			GameObject instance = Instantiate(ballPrefab);
 			Rigidbody rb = instance.GetComponent<Rigidbody>();
-			rb.velocity = Vector3.forward;
+			rb.velocity = Vector3.forward * velocity;
 		}
 		if(Input.GetKeyDown(KeyCode.DownArrow)) {
 			GameObject instance = Instantiate(ballPrefab);
 			Rigidbody rb = instance.GetComponent<Rigidbody>();
-			rb.velocity = Vector3.back;
+			rb.velocity = Vector3.back * velocity;
 		}
 		if(Input.GetKeyDown(KeyCode.LeftArrow)) {
 			GameObject instance = Instantiate(ballPrefab);
 			Rigidbody rb = instance.GetComponent<Rigidbody>();
-			rb.velocity = Vector3.left * 20.0f;
+			rb.velocity = Vector3.left * velocity;
 		}
 		if(Input.GetKeyDown(KeyCode.RightArrow)) {
 			GameObject instance = Instantiate(ballPrefab);
 			Rigidbody rb = instance.GetComponent<Rigidbody>();
-			rb.velocity = Vector3.right * 5.0f;
+			rb.velocity = Vector3.right * velocity;
 		}
 	}
 }
