daad891f6c5d4f6139f15686e147af4924924838
Royston Henson <rlqhenson@hotmail.com>
2016-12-04 13:22:53 +0000

    added mouse camera rotation

--- Assets/Scripts/BallSpawner.cs
+++ Assets/Scripts/BallSpawner.cs
@@ -11,26 +11,26 @@ public class BallSpawner : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		float velocity = 20f;
+		float speed = 20f;
 		if(Input.GetKeyDown(KeyCode.UpArrow)) {
 			GameObject instance = Instantiate(ballPrefab);
 			Rigidbody rb = instance.GetComponent<Rigidbody>();
-			rb.velocity = Vector3.forward * velocity;
+			rb.velocity = Vector3.forward * speed;
 		}
 		if(Input.GetKeyDown(KeyCode.DownArrow)) {
 			GameObject instance = Instantiate(ballPrefab);
 			Rigidbody rb = instance.GetComponent<Rigidbody>();
-			rb.velocity = Vector3.back * velocity;
+			rb.velocity = Vector3.back * speed;
 		}
 		if(Input.GetKeyDown(KeyCode.LeftArrow)) {
 			GameObject instance = Instantiate(ballPrefab);
 			Rigidbody rb = instance.GetComponent<Rigidbody>();
-			rb.velocity = Vector3.left * velocity;
+			rb.velocity = Vector3.left * speed;
 		}
 		if(Input.GetKeyDown(KeyCode.RightArrow)) {
 			GameObject instance = Instantiate(ballPrefab);
 			Rigidbody rb = instance.GetComponent<Rigidbody>();
-			rb.velocity = Vector3.right * velocity;
+			rb.velocity = Vector3.right * speed;
 		}
 	}
 }
